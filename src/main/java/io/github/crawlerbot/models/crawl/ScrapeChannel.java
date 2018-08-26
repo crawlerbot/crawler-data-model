8a69681c7c75c2df79e409c7fe9d32ad36f8f190
hungnguyen <hungnguyen0481@gmail.com>
2018-08-26 07:15:00 +0100

    update

--- src/main/java/io/github/crawlerbot/models/crawl/ScrapeChannel.java
+++ src/main/java/io/github/crawlerbot/models/crawl/ScrapeChannel.java
@@ -4,17 +4,20 @@ package io.github.crawlerbot.models.crawl;
 import io.github.crawlerbot.domain.enumeration.DestinationSystem;
 import io.github.crawlerbot.domain.enumeration.PostType;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 /**
  * A ScrapeChannel.
  */
 @Document(collection = "scrape_channel")
-@org.springframework.data.elasticsearch.annotations.Document(indexName = "scrape_channel")
+@org.springframework.data.elasticsearch.annotations.Document(indexName = "scrapechannel")
 public class ScrapeChannel implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -39,6 +42,18 @@ public class ScrapeChannel implements Serializable {
 
     @Field("post_type")
     private PostType postType;
+
+    @DBRef
+    private Set<FetchEngine> configFetchEngines = new HashSet<>();
+
+    @DBRef
+    private Set<Meta> metas = new HashSet<>();
+
+    @DBRef
+    private Set<SiteAction> siteActionConfigs = new HashSet<>();
+
+    @DBRef
+    private Set<MappingConfig> configMappings = new HashSet<>();
 
     // simlife-needle-entity-add-field - Simlife will add fields here, do not remove
     public String getId() {
@@ -125,6 +140,98 @@ public class ScrapeChannel implements Serializable {
 
     public void setPostType(PostType postType) {
         this.postType = postType;
+    }
+
+    public Set<FetchEngine> getConfigFetchEngines() {
+        return configFetchEngines;
+    }
+
+    public ScrapeChannel configFetchEngines(Set<FetchEngine> fetchEngines) {
+        this.configFetchEngines = fetchEngines;
+        return this;
+    }
+
+    public ScrapeChannel addConfigFetchEngine(FetchEngine fetchEngine) {
+        this.configFetchEngines.add(fetchEngine);
+        return this;
+    }
+
+    public ScrapeChannel removeConfigFetchEngine(FetchEngine fetchEngine) {
+        this.configFetchEngines.remove(fetchEngine);
+        return this;
+    }
+
+    public void setConfigFetchEngines(Set<FetchEngine> fetchEngines) {
+        this.configFetchEngines = fetchEngines;
+    }
+
+    public Set<Meta> getMetas() {
+        return metas;
+    }
+
+    public ScrapeChannel metas(Set<Meta> metas) {
+        this.metas = metas;
+        return this;
+    }
+
+    public ScrapeChannel addMeta(Meta meta) {
+        this.metas.add(meta);
+        return this;
+    }
+
+    public ScrapeChannel removeMeta(Meta meta) {
+        this.metas.remove(meta);
+        return this;
+    }
+
+    public void setMetas(Set<Meta> metas) {
+        this.metas = metas;
+    }
+
+    public Set<SiteAction> getSiteActionConfigs() {
+        return siteActionConfigs;
+    }
+
+    public ScrapeChannel siteActionConfigs(Set<SiteAction> siteActions) {
+        this.siteActionConfigs = siteActions;
+        return this;
+    }
+
+    public ScrapeChannel addSiteActionConfig(SiteAction siteAction) {
+        this.siteActionConfigs.add(siteAction);
+        return this;
+    }
+
+    public ScrapeChannel removeSiteActionConfig(SiteAction siteAction) {
+        this.siteActionConfigs.remove(siteAction);
+        return this;
+    }
+
+    public void setSiteActionConfigs(Set<SiteAction> siteActions) {
+        this.siteActionConfigs = siteActions;
+    }
+
+    public Set<MappingConfig> getConfigMappings() {
+        return configMappings;
+    }
+
+    public ScrapeChannel configMappings(Set<MappingConfig> mappingConfigs) {
+        this.configMappings = mappingConfigs;
+        return this;
+    }
+
+    public ScrapeChannel addConfigMapping(MappingConfig mappingConfig) {
+        this.configMappings.add(mappingConfig);
+        return this;
+    }
+
+    public ScrapeChannel removeConfigMapping(MappingConfig mappingConfig) {
+        this.configMappings.remove(mappingConfig);
+        return this;
+    }
+
+    public void setConfigMappings(Set<MappingConfig> mappingConfigs) {
+        this.configMappings = mappingConfigs;
     }
     // simlife-needle-entity-add-getters-setters - Simlife will add getters and setters here, do not remove
 
